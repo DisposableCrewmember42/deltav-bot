f667dc5a8ebb8794cefe75a77a0cb84b1e5edd66
DisposableCrewmember42 <disposablecrewmember42@proton.me>
2026-03-27 19:55:51 +0100

    refactor: rename get_env to required_env

--- deltav-bot/src/main.rs
+++ deltav-bot/src/main.rs
@@ -8,7 +8,7 @@ mod direction;
 mod github;
 mod relays;
 
-macro_rules! get_env {
+macro_rules! required_env {
     ($variable_name: expr, $local_var: ident) => {
         let val: &str = $variable_name;
         let Ok($local_var) = std::env::var(val) else {
@@ -22,31 +22,31 @@ macro_rules! get_env {
 async fn main() {
     tracing_subscriber::fmt().init();
 
-    get_env!("GH_REPO_OWNER", repo_owner);
-    get_env!("GH_REPO_NAME", repo_name);
-    get_env!("GH_APP_KEY", app_key);
+    required_env!("GH_REPO_OWNER", repo_owner);
+    required_env!("GH_REPO_NAME", repo_name);
+    required_env!("GH_APP_KEY", app_key);
     let Ok(app_key) = EncodingKey::from_rsa_pem(app_key.as_bytes()) else {
         error!("Invalid GitHub Application Key. Must be an RSA key in PEM format.");
         return;
     };
-    get_env!("GH_APP_ID", app_id);
+    required_env!("GH_APP_ID", app_id);
     let Ok(app_id) = app_id.parse::<u64>() else {
         error!("Invalid GitHub Application ID. Must be u64.");
         return;
     };
 
-    get_env!("WEBHOOK_PORT", webhook_port);
+    required_env!("WEBHOOK_PORT", webhook_port);
     let Ok(webhook_port) = webhook_port.parse::<u16>() else {
         error!("Invalid Webhook port. Must be u16.");
         return;
     };
-    get_env!("WEBHOOK_SECRET", webhook_secret);
+    required_env!("WEBHOOK_SECRET", webhook_secret);
     if webhook_secret.len() < 16 {
         error!("The Webhook secret must be at least 16 characters long.");
         return;
     }
 
-    get_env!("DISCORD_TOKEN", discord_token);
+    required_env!("DISCORD_TOKEN", discord_token);
 
     let Ok(mut gh) = GitHubService::initialize(
         webhook_port,
